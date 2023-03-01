95798c9effa0c0ee283f37549b92c828db4dac86
Alec Livaditis <aleclivaditis@gmail.com>
2023-03-01 13:54:53 -0500

    Fix playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,7 @@
 // first make a fucntion called getComputerChoice
 
 function getComputerChoice() {
-    computerChoice = " "
+    computerChoice = ""
     var randomChoice = Math.floor(Math.random()*3);
     if (randomChoice === 0) {
         computerChoice = "rock";
@@ -20,22 +20,19 @@ function getComputerChoice() {
 //playerSelection parameter will be case insensitive
 
 function playRound(playerSelection, computerSelection) {
-    returnString = " "
+    
     if (playerSelection === computerSelection) {
-        returnString = "It is a tie!";
+        return "It is a tie!";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        returnString = "You win! Rock beats scissors!";
+        return "You win! Rock beats scissors!";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        returnString = "You win! Scissors beats paper!";
+        return "You win! Scissors beats paper!";
     } else if (playerSelection === "paper" && computerSelection === "rock"){
-        returnString = "You win! Paper beats rock!";
+        return "You win! Paper beats rock!";
     } else {
-        returnString = "You lose!";
+        return "You lose!";
     } 
-    return returnString;
-    console.log(returnString);
 }
-
 
 
 //Make a function called game() that calls playRound()
