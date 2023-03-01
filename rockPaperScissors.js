581a1cbbcb517e95ae25dacd5b9acadb26e540d7
Alec Livaditis <aleclivaditis@gmail.com>
2023-03-01 13:17:08 -0500

    Add playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,7 @@
 // first make a fucntion called getComputerChoice
 
 function getComputerChoice() {
-    computerChoice = ""
+    computerChoice = " "
     var randomChoice = Math.floor(Math.random()*3);
     if (randomChoice === 0) {
         computerChoice = "rock";
@@ -11,12 +11,32 @@ function getComputerChoice() {
         computerChoice = "scissors";
     }
     console.log(`The computer chose ${computerChoice}!`);
+    return computerChoice;
 }
 
 //Write a function that plays a single round
 //Two parameters, playerSelection, computerSelection
 //Return a string that declares the winner and why
 //playerSelection parameter will be case insensitive
+
+function playRound(playerSelection, computerSelection) {
+    returnString = " "
+    if (playerSelection === computerSelection) {
+        returnString = "It is a tie!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        returnString = "You win! Rock beats scissors!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        returnString = "You win! Scissors beats paper!";
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        returnString = "You win! Paper beats rock!";
+    } else {
+        returnString = "You lose!";
+    } 
+    return returnString;
+    console.log(returnString);
+}
+
+
 
 //Make a function called game() that calls playRound()
 //play 5 games with a for loop
